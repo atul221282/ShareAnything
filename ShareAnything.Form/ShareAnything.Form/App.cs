5f31ce8c7f3f2e4be117e2426992a3e9d3932569
Atul Chaudhary <atul221282@gmail.com>
2016-05-31 07:54:13 +0930

    more changes

--- ShareAnything.Form/ShareAnything.Form/App.cs
+++ ShareAnything.Form/ShareAnything.Form/App.cs
@@ -17,7 +17,7 @@ namespace ShareAnything.Form
 					VerticalOptions = LayoutOptions.Center,
 					Children = {
 						new Label {
-							XAlign = TextAlignment.Center,
+							HorizontalTextAlignment = TextAlignment.Center,
 							Text = "Welcome to Xamarin Forms!"
 						}
 					}
