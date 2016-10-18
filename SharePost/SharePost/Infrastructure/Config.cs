7bc1aac7760b282767eb8ea7dfc5c2ff6837fc20
atul <atul221282@gmail.com>
2016-10-18 19:52:15 +1030

    hi

---
+++ SharePost/SharePost/Infrastructure/Config.cs
@@ -0,0 +1,25 @@
+﻿using Ninject.Modules;
+using SharePost.Contracts;
+using SharePost.Helpers;
+using SharePost.Service;
+using SharePost.Service.Comon;
+using SharePost.ServiceContract;
+using SharePost.ServiceContract.Common;
+using SharePost.ViewModel.Account;
+using System;
+using XLabs.Ioc;
+using Ninject;
+
+namespace SharePost.Infrastructure
+{
+    public class Config : NinjectModule
+    {
+        
+
+        public override void Load()
+        {
+            Bind<ILoginService>().To<LoginService>();
+            Bind<ILoginViewModel>().To<LoginViewModel>();
+        }
+    }
+}
---
+++ SharePost/SharePost/ViewModel/Account/ILoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Plugin.Geolocator.Abstractions;
+using System.Threading.Tasks;
+
+namespace SharePost.ViewModel.Account
+{
+    public interface ILoginViewModel
+    {
+        Position Position { get; set; }
+        string UserName { get; set; }
+        string Password { get; set; }
+        bool IsNotLoading { get; set; }
+        Task Login();
+    }
+}
